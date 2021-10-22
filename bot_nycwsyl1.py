a907bcba19bdf049b231434b3f80d4395d6d4be3
dogukannulu <dogukannulu@gmail.com>
2021-10-22 22:41:59 +0200

    Final-2 Commit

--- bot_nycwsyl1.py
+++ bot_nycwsyl1.py
@@ -35,13 +35,11 @@ text2 = total_text[total_text.find("Down"):]
 def visualize(my_text):
     """ Web sayfasından alınan bölümü istenen şekilde yazdıran fonksiyon"""
     my_new_text = ''
-    for i in my_text:
-        if i.isnumeric() and my_text.index(i)!=0:
-            my_new_text += '\n' + i + '.' + ' '
-        elif i.isnumeric() and my_text.index(i)==0:
-            my_new_text += i + '.' + ' '
-        elif not i.isnumeric():
-            my_new_text += i
+    for i in range(len(my_text)):
+        if my_text[i].isnumeric() and my_text[i-1].isnumeric()==False:
+            my_new_text += '\n' + my_text[i] + '.' + ' '
+        else:
+            my_new_text += my_text[i]
     logging.info(my_new_text)
     print(my_new_text)
 
